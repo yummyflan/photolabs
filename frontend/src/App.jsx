e7345db4c0e960d1163b94fd8e5d74d310a1fe52
Alanna <phan.alanna@gmail.com>
2023-11-05 18:51:54 -0500

    Add logo, topics, and favourite button to nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.scss';
 import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
+import TopNavigationBar from 'components/TopNavigationBar';
 // Note: Rendering a single component to build components in isolation
 
 const App = () => {
   return (
     <div className="App">
-      <TopicList />
+      <TopNavigationBar />
       <PhotoList />
     </div>
   );
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-
+import TopicList from './TopicList';
+import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigation = () => {
+const TopNavigationBar = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <TopicList />
+      <FavBadge />
     </div>
   )
 }
 
-export default TopNavigation;
+export default TopNavigationBar;
